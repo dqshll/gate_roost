74e9731fd257c05779839a14980ecdcd93d659a1
dingqun <qun.ding@edisonx.cn>
2018-09-05 18:26:02 +0800

    somthing done for roost

--- Applications/Roost/Web/ws.py
+++ Applications/Roost/Web/ws.py
@@ -2,7 +2,7 @@ import sys
 from websocket import create_connection
 
 ws = create_connection("ws://api.edisonx.cn:3340")
-ws.send("trg_" + sys.sys.argv[1])
+ws.send("trg_" + sys.argv[1])
 result =  ws.recv()
 #print("Received '%s'" % result)
 ws.close()
